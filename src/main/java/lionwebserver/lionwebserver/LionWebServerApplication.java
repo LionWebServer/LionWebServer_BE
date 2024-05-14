9d57443855aa642165de9a18462d9c0e99939afc
Dohun Kim <dohun8758@naver.com>
2024-05-15 01:36:59 +0900

    init: mainˆ

---
+++ src/main/java/lionwebserver/lionwebserver/LionWebServerApplication.java
@@ -0,0 +1,13 @@
+package lionwebserver.lionwebserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LionWebServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LionWebServerApplication.class, args);
+    }
+
+}
