603a50963b23755cbcf7ed079f2acaa8783bb9b8
Dohun Kim <dohun8758@naver.com>
2024-05-16 23:21:54 +0900

    feat: 전역 예외 처리기 구현

---
+++ src/main/java/lionwebserver/lionwebserver/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package lionwebserver.lionwebserver.exception;
+
+import lionwebserver.lionwebserver.exception.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<ErrorResponse> handleBaseException(BaseException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        ErrorResponse errorResponse = new ErrorResponse(
+                errorCode.getStatusCode(),
+                errorCode.getExceptionCode(),
+                errorCode.getMessage(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity
+                .status(HttpStatus.valueOf(errorCode.getStatusCode()))
+                .body(errorResponse);
+    }
+}
+
