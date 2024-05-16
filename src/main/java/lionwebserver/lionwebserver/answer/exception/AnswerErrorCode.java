53729151bf88a92e2f679383fcb3da2455c442d2
Dohun Kim <dohun8758@naver.com>
2024-05-16 23:17:40 +0900

    feat: 답변 에러코드

---
+++ src/main/java/lionwebserver/lionwebserver/answer/exception/AnswerErrorCode.java
@@ -0,0 +1,21 @@
+package lionwebserver.lionwebserver.answer.exception;
+
+import lionwebserver.lionwebserver.exception.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public enum AnswerErrorCode implements ErrorCode {
+    USER_NOT_FOUND(3000, 404, "사용자를 찾을 수 없습니다."),
+    QUESTION_NOT_FOUND(3001, 404, "질문을 찾을 수 없습니다.");
+
+
+    private final int exceptionCode;
+    private final int statusCode;
+    private final String message;
+
+    AnswerErrorCode(int exceptionCode, int statusCode, String message) {
+        this.exceptionCode = exceptionCode;
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+}
