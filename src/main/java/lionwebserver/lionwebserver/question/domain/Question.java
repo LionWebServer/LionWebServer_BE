d4f7351e799fa2aab8236478578ef2721cf4a214
Dohun Kim <dohun8758@naver.com>
2024-05-16 23:22:37 +0900

    feat: 질문 엔티티에 builder 추가

--- src/main/java/lionwebserver/lionwebserver/question/domain/Question.java
+++ src/main/java/lionwebserver/lionwebserver/question/domain/Question.java
@@ -3,7 +3,9 @@ package lionwebserver.lionwebserver.question.domain;
 import jakarta.persistence.*;
 import lionwebserver.lionwebserver.answer.domain.Answer;
 import lionwebserver.lionwebserver.auth.domain.User;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -30,4 +32,14 @@ public class Question {
 
     @OneToMany(mappedBy = "question", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Answer> answers;
+
+    @Builder
+    public Question(String title, String content, LocalDateTime createdAt, User user) {
+        this.title = title;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.user = user;
+    }
+    protected Question() {
+    }
 }
