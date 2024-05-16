bb028b3396f87cf4b21effa0784ff68fdac8352b
Dohun Kim <dohun8758@naver.com>
2024-05-16 23:23:01 +0900

    feat: QuestionController 추가 및 질문 생성, 질문 상세 조회, 질문 리스트 조회 api 추가

---
+++ src/main/java/lionwebserver/lionwebserver/question/controller/QuestionController.java
@@ -0,0 +1,37 @@
+package lionwebserver.lionwebserver.question.controller;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lionwebserver.lionwebserver.auth.support.AuthUser;
+import lionwebserver.lionwebserver.question.controller.dto.*;
+import lionwebserver.lionwebserver.question.service.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class QuestionController {
+
+    private final QuestionService questionService;
+    @PostMapping("/api/question")
+    public ResponseEntity<?> createQuestion(QuestionCreateDTO questionCreateDTO,
+                                             @Parameter(hidden = true) @AuthUser Long userId) {
+        questionService.createQuestion(questionCreateDTO, userId);
+        return ResponseEntity.ok().body("질문이 등록되었습니다.");
+    }
+
+    @GetMapping("/api/question")
+    public ResponseEntity<QuestionDTO> getQuestions(Long questionId) {
+        return ResponseEntity.ok().body(questionService.getQuestion(questionId));
+    }
+    @GetMapping("/api/question-list")
+    public ResponseEntity<QuestionPageResponse> getQuestions(PaginationDTO paginationDTO) {
+
+        return ResponseEntity.ok().body(questionService.getQuestions(paginationDTO));
+    }
+}
