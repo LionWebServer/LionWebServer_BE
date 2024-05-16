852fdee321cdd6357f8d1c1a53decbdf017c40dc
Dohun Kim <dohun8758@naver.com>
2024-05-16 23:17:14 +0900

    feat: AnswerController 생성 및 답변 생성, 답변리스트 조회 api 구현

---
+++ src/main/java/lionwebserver/lionwebserver/answer/controller/AnswerController.java
@@ -0,0 +1,30 @@
+package lionwebserver.lionwebserver.answer.controller;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import lionwebserver.lionwebserver.answer.controller.dto.AnswerCreateDTO;
+import lionwebserver.lionwebserver.answer.service.AnswerService;
+import lionwebserver.lionwebserver.auth.support.AuthUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AnswerController {
+
+    private final AnswerService answerService;
+    @PostMapping("/api/answer")
+    public ResponseEntity<?> createAnswer(AnswerCreateDTO answerCreateDTO,
+                                          @Parameter(hidden = true) @AuthUser Long userId) {
+        answerService.createAnswer(answerCreateDTO, 1L);
+        return ResponseEntity.ok().body("답변이 등록되었습니다.");
+    }
+
+    @GetMapping("/api/answer-list")
+    public ResponseEntity<?> getAnswers(Long questionId) {
+        return ResponseEntity.ok().body(answerService.getAnswers(questionId));
+    }
+
+}
