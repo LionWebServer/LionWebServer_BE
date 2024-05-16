407f5d730403c5f2707980e7acdd227483b956c7
Dohun Kim <dohun8758@naver.com>
2024-05-16 23:25:59 +0900

    feat: UserRepository 추가

---
+++ src/main/java/lionwebserver/lionwebserver/auth/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package lionwebserver.lionwebserver.auth.repository;
+
+import lionwebserver.lionwebserver.auth.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserEmail(String userEmail);
+}
