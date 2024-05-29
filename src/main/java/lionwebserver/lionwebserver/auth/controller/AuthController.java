83ce8a4a9d95ed1fba201b16f1afc04e54a12c61
Dohun Kim <dohun8758@naver.com>
2024-05-29 16:14:16 +0900

    feat: 어노테이션 추가

--- src/main/java/lionwebserver/lionwebserver/auth/controller/AuthController.java
+++ src/main/java/lionwebserver/lionwebserver/auth/controller/AuthController.java
@@ -7,6 +7,7 @@ import lionwebserver.lionwebserver.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,13 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthService authService;
     @PostMapping("/api/auth/sign-up")
-    public ResponseEntity<?> signUp(SignUpDTO signUpDTO) {
+    public ResponseEntity<?> signUp(@RequestBody SignUpDTO signUpDTO) {
         authService.signUp(signUpDTO);
         return ResponseEntity.ok().body("회원가입이 완료되었습니다.");
     }
 
     @PostMapping("/api/auth/sign-in")
-    public ResponseEntity<TokenResponse> signIn(SignInDTO signInDTO) {
+    public ResponseEntity<TokenResponse> signIn(@RequestBody SignInDTO signInDTO) {
         return ResponseEntity.ok(authService.signIn(signInDTO));
     }
 
