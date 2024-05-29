64d5c6ed0379cf92df6d9d0b4c483f42fc0c0cb5
Dohun Kim <dohun8758@naver.com>
2024-05-29 16:14:13 +0900

    feat: 어노테이션 추가

--- src/main/java/lionwebserver/lionwebserver/answer/controller/AnswerController.java
+++ src/main/java/lionwebserver/lionwebserver/answer/controller/AnswerController.java
@@ -7,9 +7,7 @@ import lionwebserver.lionwebserver.answer.service.AnswerService;
 import lionwebserver.lionwebserver.auth.support.AuthUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,14 +17,14 @@ public class AnswerController {
 
     private final AnswerService answerService;
     @PostMapping("/api/answer")
-    public ResponseEntity<?> createAnswer(AnswerCreateDTO answerCreateDTO,
+    public ResponseEntity<?> createAnswer(@RequestBody AnswerCreateDTO answerCreateDTO,
                                           @Parameter(hidden = true) @AuthUser Long userId) {
         answerService.createAnswer(answerCreateDTO, 1L);
         return ResponseEntity.ok().body("답변이 등록되었습니다.");
     }
 
-    @GetMapping("/api/answer-list")
-    public ResponseEntity<List<AnswerDTO>> getAnswers(Long questionId) {
+    @GetMapping("/api/answer-list/{questionId}")
+    public ResponseEntity<List<AnswerDTO>> getAnswers(@PathVariable Long questionId) {
         return ResponseEntity.ok().body(answerService.getAnswers(questionId));
     }
 }
